8b09fcf51a596ae12b11667251a97090812544a8
Clément Lemoine <clementlemoine.cl@gmail.com>
2018-05-29 16:39:47 +0200

    🎨 footer wp

--- src/theme/footer.php
+++ src/theme/footer.php
@@ -2,32 +2,47 @@
 
         <footer class='footer' role='contentinfo'>
             <div class='container'>
-                <div class='top'>
-                    <section class='footer-category'>
-                        <h4 class='category-title'>R&D and Products</h4>
-                        <ul>
-                            <li><a href='#'>Technology</a></li>
-                            <li><a href='#'>Neptune</a></li>
-                            <li><a href='#'>Nemo</a></li>
-                        </ul>
-                    </section>
-                    <section class='footer-category big'>
-                        <h4 class='category-title'>Science</h4>
-                        <ul>
-                            <li><a href='#'>Technology</a></li>
-                            <li><a href='#'>Neptune</a></li>
-                            <li><a href='#'>Nemo</a></li>
-                        </ul>
-                    </section>
-                    <section class='footer-category'>
-                        <h4 class='category-title'>The company</h4>
-                        <ul>
-                            <li><a href='#'>Technology</a></li>
-                            <li><a href='#'>Neptune</a></li>
-                            <li><a href='#'>Nemo</a></li>
-                        </ul>
-                    </section>
-                </div>
+                
+                    <?php if( have_rows('website_footer', 'options') ): ?>
+                        <div class='top'>
+
+                            <?php 
+                                while ( have_rows('website_footer', 'options') ) : the_row();
+                                $class = get_sub_field('footer_double') ? 'footer-category big' : 'footer-category';
+                            ?>
+
+                                <section class='<?php echo $class; ?>'>
+
+                                    <h4 class='category-title'><?php the_sub_field('footer_column_title') ?></h4>
+
+                                    <?php if( have_rows('footer_column_links') ): ?>
+                                        <ul>
+                                            <?php 
+                                                while ( have_rows('footer_column_links') ) : the_row();
+                                                $link = get_sub_field('footer_column_link');
+                                                    if($link):
+                                            ?>
+
+                                                        <li>
+                                                            <a href='<?php echo $link['url'] ?>' title='<?php echo $link['title'] ?>'><?php echo $link['title'] ?></a>
+                                                        </li>
+
+
+                                            <?php 
+                                                    endif;
+                                                endwhile; 
+                                            ?>
+                                        </ul>
+                                    <?php endif; ?>
+
+
+                               </section>
+                       
+                           <?php endwhile; ?>
+                        </div>
+                    <?php 
+                       endif;
+                    ?>
                 <aside class='legal'>
                     <span>© Copyright CorWave SA 2018 - <a href='#'>Legal Notice</a></span>
                 </aside>
