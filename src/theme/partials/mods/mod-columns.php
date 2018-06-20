246cc6bee887fccd97a2951cabc2c82dd3829543
Alphability <albanmezino@gmail.com>
2018-06-20 17:56:44 +0200

    🎨columns style & responsive
    Signed-off-by: Alphability <albanmezino@gmail.com>

--- src/theme/partials/mods/mod-columns.php
+++ src/theme/partials/mods/mod-columns.php
@@ -2,42 +2,43 @@
     // $columns_row = get_sub_field('columns_row');
 ?>
     <section class='module-columns pb'>
-        <div class='container-columns container'>
-            <div class='container-small'>
-                <?php 
-                $introduction = get_sub_field('introduction');
-                $intro_txt = get_sub_field('columns_introduction');
-                if ($introduction && $intro_txt) : 
-                    echo $intro_txt;
-                endif;
-                if( have_rows('columns_row') ):
-                    while ( have_rows('columns_row') ) : the_row();
-                        $columns_number = get_sub_field('columns_number') ? intval(get_sub_field('columns_number')) : false;
+        <div class="container-columns container-big">
+            <?php
+            $introduction = get_sub_field('introduction');
+            $intro_txt = get_sub_field('columns_introduction');
+            if ($introduction && $intro_txt) :
+            ?>
+                <div class='introduction'>
+                    <?php echo $intro_txt ?>
+                </div>
+            <? endif;
+            if( have_rows('columns_row') ):
+                while ( have_rows('columns_row') ) : the_row();
+                    $columns_number = get_sub_field('columns_number') ? intval(get_sub_field('columns_number')) : false;
 
-                        $first_col = get_sub_field('first_column');
-                        $second_col = get_sub_field('second_column');
-                        $third_col = get_sub_field('third_column');
-                        $fourth_col = get_sub_field('fourth_column');
+                    $first_col = get_sub_field('first_column');
+                    $second_col = get_sub_field('second_column');
+                    $third_col = get_sub_field('third_column');
+                    $fourth_col = get_sub_field('fourth_column');
 
-                        $columns = array(
-                            $first_col,
-                            $second_col,
-                        );
-                        if ($columns_number === 3) {
-                            $columns[] = $third_col;
-                        } else if ($columns_number === 4) {
-                            $columns[] = $third_col;
-                            $columns[] = $fourth_col;
-                        }
-                ?>
-                <div class="columns <?php echo $columns_number === 3 ? ' third' : '' ?><?php echo $columns_number === 4 ? ' fourth' : '' ?>">
-                    <?php foreach ($columns as $col) : ?>
-                    <div class="column">
-                        <div class='column-content'>
-                            <?php echo $col['column_content']; ?>
-                        </div>                    
-
-                        <?php if( $col['column_links']): ?>
+                    $columns = array(
+                        $first_col,
+                        $second_col,
+                    );
+                    if ($columns_number === 3) {
+                        $columns[] = $third_col;
+                    } else if ($columns_number === 4) {
+                        $columns[] = $third_col;
+                        $columns[] = $fourth_col;
+                    }
+            ?>
+            <div class="columns <?php echo $columns_number === 3 ? ' third' : '' ?><?php echo $columns_number === 4 ? ' fourth' : '' ?> <?php echo $second_col === '' ? 'solo' : '' ?>">
+                <?php foreach ($columns as $col) : ?>
+                <div class="column">
+                    <div class='column-content'>
+                        <?php echo $col['column_content']; ?>
+                    </div>
+                    <?php if( $col['column_links']): ?>
                             <div class='column-links'>
                                 <?php 
                                     foreach ($col['column_links'] as $linkArray) : 
@@ -47,13 +48,12 @@
                                 <?php endforeach; ?>
                             </div>
                         <?php endif; ?>
-                    </div>
-                    <?php endforeach; ?>
                 </div>
-                <?php 
-                    endwhile;
-                endif;
-                ?>
+                <?php endforeach; ?>
             </div>
+            <?php
+                endwhile;
+            endif;
+            ?>
         </div>
     </section>
