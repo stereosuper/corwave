e6317fa5d44727634de28e4681fcfdd6adb9e9a6
Alphability <albanmezino@gmail.com>
2018-06-22 11:42:22 +0200

    🎨home responsive
    Signed-off-by: Alphability <albanmezino@gmail.com>

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,11 +1,25 @@
 const $ = require('jquery-slim');
 
-module.exports = function( header, burger ){
+module.exports = function burgerMenu(header, burger) {
+    if (!burger.length) return;
 
-    if( !burger.length ) return;
-
-    burger.on('click', function(e){
+    burger.on('click', (e) => {
         e.preventDefault();
-        header.toggleClass('menu-open');
+
+        if (header.hasClass('menu-open')) {
+            const menuItemsWithChildren = $('.menu-item-has-children');
+
+            TweenLite.to($('.menu-item-has-children.on').find('.sub-menu-wrap'), 0.3, {
+                css: { maxHeight: 0 },
+                ease: Power4.easeOut,
+                onComplete: () => {
+                    header.toggleClass('menu-open');
+                },
+            });
+
+            menuItemsWithChildren.removeClass('on').removeClass('was-on');
+        } else {
+            header.toggleClass('menu-open');
+        }
     });
-}
+};
--- src/js/mainMenu.js
+++ src/js/mainMenu.js
@@ -17,44 +17,52 @@ module.exports = function mainMenu(header, menu) {
         e.preventDefault();
         header.removeClass('on');
     }).on('click', '.menu-item-has-children > a, .menu-item-has-children > span', function itemClick(e) {
-        let easing = Expo.easeOut;
         const parents = $('.menu-item-has-children');
+        const easing = Power4.easeInOut;
+
         if (win.w <= 960) {
             e.preventDefault();
             liParent = $(this).parents('.menu-item-has-children');
-            if (liParent.hasClass('on')) {
-                return;
-            }
+
             parents.each((index, parent) => {
-                if ($(parent).hasClass('on')) {
+                if ($(parent).not(liParent).hasClass('on')) {
                     $(parent).addClass('was-on');
                     $(parent).removeClass('on');
                 }
             });
+
             liParent.toggleClass('on');
             if (liParent.hasClass('on')) {
                 maxH = 500;
-                easing = Expo.easeIn;
+                // easing = Power4.easeIn;
             } else {
                 maxH = 0;
-                easing = Expo.easeOut;
+                // easing = Power4.easeOut;
             }
-            TweenLite.to($('.menu-item-has-children.was-on').find('.sub-menu-wrap'), 0.3, {
+
+            TweenLite.to($('.menu-item-has-children.was-on').find('.sub-menu-wrap'), 0.5, {
                 css: { maxHeight: 0 },
-                ease: Expo.easeOut,
+                ease: Power4.easeInOut,
                 onComplete: () => {
-                    TweenLite.to(liParent.find('.sub-menu-wrap'), 0.3, { css: { maxHeight: maxH }, ease: easing });
+                    TweenLite.to(liParent.find('.sub-menu-wrap'), 0.5, { css: { maxHeight: maxH }, ease: easing });
                 },
             });
+
             parents.removeClass('was-on');
         }
     });
 
     const resizeHandler = () => {
-        if (win.h == $(window).height()) {
+        if (win.h === $(window).height()) {
             TweenLite.set($('.menu-item-has-children.on .sub-menu-wrap'), { clearProps: 'all' });
             $('.menu-item-has-children.on').removeClass('on');
             $('header.menu-open').removeClass('menu-open');
+
+            if (win.w > 960) {
+                $('.menu-item-has-children').find('.sub-menu-wrap').css({ maxHeight: 'none' });
+            } else {
+                $('.menu-item-has-children').find('.sub-menu-wrap').css({ maxHeight: '0' });
+            }
         }
     };
 
