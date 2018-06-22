f83c17a6c96d1ec35128d51c33a54129943095c4
Alphability <albanmezino@gmail.com>
2018-06-22 14:37:32 +0200

    🎨animations slider & header
    Signed-off-by: Alphability <albanmezino@gmail.com>

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,9 +1,10 @@
 const $ = require('jquery-slim');
 const io = require('./io');
 
-require('gsap');
-const CSSRulePlugin = require('gsap/CSSRulePlugin');
 require('hammerjs');
+require('gsap/TweenLite');
+const CSSRulePlugin = require('gsap/CSSRulePlugin');
+const CustomEase = require('./plugins/CustomEase');
 
 module.exports = function slider(slider, auto = false) {
     if (!slider.length) return;
@@ -16,20 +17,22 @@ module.exports = function slider(slider, auto = false) {
     let index = 0;
 
     const showSlide = (bulletIndex) => {
-        index = typeof bulletIndex === 'number' ? bulletIndex : bulletIndex.data('slide');
+        index =
+            typeof bulletIndex === 'number'
+                ? bulletIndex
+                : bulletIndex.data('slide');
 
         const prevSlide = slider.find('.slide.active');
         const prevBullet = slider.find('.bullet.active');
         const slide = slider.find(`.slide[data-slide=${index}]`);
         const slideSide = slide.find('.left-side,.txt-side');
         const slideSideBackground = slideSide.find('.slide-layer-background');
-        const sliderInnerTxtSide = slide.find('.inner-left-side, .inner-txt-side');
+        const sliderInnerTxtSide = slide.find('.inner-left-side, .inner-txt-side',);
 
         const bullet = slider.find(`.bullet[data-slide=${index}]`);
 
         if (bullet.index() === prevBullet.index()) return;
         const reverse = bullet.index() < prevBullet.index() ? -1 : 1;
-
 
         prevBullet.removeClass('active');
         prevSlide.removeClass('active');
@@ -38,44 +41,56 @@ module.exports = function slider(slider, auto = false) {
         slide.removeClass('was-active');
         slide.addClass('active');
 
-        TweenMax.set(slideSideBackground, { xPercent: 100 * reverse, skewX: 0 });
-        TweenMax.set(sliderInnerTxtSide, { opacity: 0 });
-
-        TweenMax.fromTo(slideSideBackground, 0.5, {
+        TweenLite.set(slideSideBackground, {
             xPercent: 100 * reverse,
-            transformOrigin: '0% 50%',
-        }, {
-            xPercent: 0,
-            ease: Expo.easeOut,
-            onComplete() {
-                TweenMax.to(sliderInnerTxtSide, 0.8, {
-                    opacity: 1,
-                    ease: Expo.easeOut,
-                });
-
-                // io.ohTitleIn(sliderInnerTxt);
-                // prevSlide.removeClass('was-active');
-            },
+            skewX: 0,
         });
+        TweenLite.set(sliderInnerTxtSide, { opacity: 0 });
 
+        TweenLite.fromTo(
+            slideSideBackground,
+            0.8,
+            {
+                xPercent: 100 * reverse,
+                transformOrigin: '0% 50%',
+            },
+            {
+                xPercent: 0,
+                // ease: CustomEase.create("custom", "M0,0 C0.15,1.02 0.25,1.03 1,1"),
+                ease: CustomEase.create("custom", "M0,0 C0.75,-0.03 0.85,0 1,1"),
+                onComplete() {
+                    TweenLite.to(sliderInnerTxtSide, 0.8, {
+                        opacity: 1,
+                        ease: CustomEase.create("custom", "M0,0 C0.15,1.02 0.25,1.03 1,1"),
+                    });
+                },
+            },
+        );
 
-        if (auto && !stopSlider) TweenMax.delayedCall(slideDelay, showSlide, [bullet.next().length ? bullet.next() : firstBullet]);
+        if (auto && !stopSlider)
+            {TweenLite.delayedCall(slideDelay, showSlide, [
+                bullet.next().length ? bullet.next() : firstBullet
+            ]);}
     };
 
     slider.on('click', '.bullet', function () {
-        TweenMax.killDelayedCallsTo(showSlide);
-        TweenMax.killTweensOf(showSlide);
+        TweenLite.killDelayedCallsTo(showSlide);
+        TweenLite.killTweensOf(showSlide);
         showSlide($(this));
     });
 
     slider.on('mouseenter', () => {
-        TweenMax.killDelayedCallsTo(showSlide);
+        TweenLite.killDelayedCallsTo(showSlide);
         stopSlider = true;
     });
 
     slider.on('mouseleave', () => {
         stopSlider = false;
-        const newIndex = slider.find(`.slide[data-slide=${parseInt(index, 10) + 1}]`).length !== 0 ? slider.find(`.slide[data-slide=${parseInt(index, 10) + 1}]`) : 0;
+        const newIndex =
+            slider.find(`.slide[data-slide=${parseInt(index, 10) + 1}]`)
+                .length !== 0
+                ? slider.find(`.slide[data-slide=${parseInt(index, 10) + 1}]`)
+                : 0;
         if (auto) {
             setTimeout(() => {
                 showSlide(newIndex);
@@ -83,29 +98,38 @@ module.exports = function slider(slider, auto = false) {
         }
     });
 
-    if (auto && !stopSlider) TweenMax.delayedCall(7, showSlide, [firstBullet.next()]);
+    if (auto && !stopSlider)
+        {TweenLite.delayedCall(7, showSlide, [firstBullet.next()]);}
 
     const setSwipe = (slider) => {
         if (swipeInstance !== null) return;
 
-    	swipeInstance = new Hammer(slider);
+        swipeInstance = new Hammer(slider);
 
-        swipeInstance.on('swipeleft', () => {
-		    auto ? TweenMax.killDelayedCallsTo(showSlide) : TweenMax.killTweensOf(showSlide);
+        swipeInstance
+            .on('swipeleft', () => {
+                auto
+                    ? TweenLite.killDelayedCallsTo(showSlide)
+                    : TweenLite.killTweensOf(showSlide);
 
-		    if (parseInt(index) + 1 < length) {
-		    	showSlide(parseInt(index) + 1);
-		    }
-        }).on('swiperight', () => {
-		    auto ? TweenMax.killDelayedCallsTo(showSlide) : TweenMax.killTweensOf(showSlide);
+                if (parseInt(index) + 1 < length) {
+                    showSlide(parseInt(index) + 1);
+                }
+            })
+            .on('swiperight', () => {
+                auto
+                    ? TweenLite.killDelayedCallsTo(showSlide)
+                    : TweenLite.killTweensOf(showSlide);
 
-		    if (parseInt(index) > 0) {
-		    	showSlide(parseInt(index) - 1);
-		    }
-        });
+                if (parseInt(index) > 0) {
+                    showSlide(parseInt(index) - 1);
+                }
+            });
 
         swipeInstance.on('press', () => {
-            auto ? TweenMax.killDelayedCallsTo(showSlide) : TweenMax.killTweensOf(showSlide);
+            auto
+                ? TweenLite.killDelayedCallsTo(showSlide)
+                : TweenLite.killTweensOf(showSlide);
         });
 
         swipeInstance.on('pressup', () => {
@@ -116,8 +140,8 @@ module.exports = function slider(slider, auto = false) {
     const destroySwipe = () => {
         if (!swipeInstance) return;
 
-    	swipeInstance.destroy();
-    	swipeInstance = null;
+        swipeInstance.destroy();
+        swipeInstance = null;
     };
 
     window.outerWidth > 780 ? destroySwipe() : setSwipe(slider[0]);
