e881c3f62a9b6847f239b6c4968ba5809a99fd1d
Alphability <albanmezino@gmail.com>
2018-07-26 18:13:00 +0200

    🎨map useless code removed
    Signed-off-by: Alphability <albanmezino@gmail.com>

--- src/theme/contact.php
+++ src/theme/contact.php
@@ -366,169 +366,6 @@ get_header(); ?>
 		google.maps.event.addDomListener(window, 'load', init);
 		let map,
 			markersArray = [];
-		function bindInfoWindow(marker, map, location) {
-			google.maps.event.addListener(marker, 'click', () => {
-				// function close(location) {
-				// 	location.ib.close();
-				// 	location.infoWindowVisible = false;
-				// 	location.ib = null;
-				// }
-				if (location.infoWindowVisible === true) {
-					// close(location);
-				} else {
-					// markersArray.forEach(function(loc, index) {
-					// 	if (loc.ib && loc.ib !== null) {
-					// 		close(loc);
-					// 	}
-					// });
-					var boxText = document.createElement('div');
-					boxText.style.cssText = 'background: #fff;';
-					boxText.classList.add('md-whiteframe-2dp');
-
-					function buildPieces(location, el, part, icon) {
-						if (location[part] === '') {
-							return '';
-						} else if (location.iw[part]) {
-							switch (el) {
-								case 'photo':
-									if (location.photo) {
-										return (
-											'<div class="iw-photo" style="background-image: url(' +
-											location.photo +
-											');"></div>'
-										);
-									} else {
-										return '';
-									}
-									break;
-								case 'iw-toolbar':
-									return (
-										'<div class="iw-toolbar"><h3 class="md-subhead">' +
-										location.title +
-										'</h3></div>'
-									);
-									break;
-								case 'div':
-									switch (part) {
-										case 'email':
-											return (
-												'<div class="iw-details"><i class="material-icons" style="color:#4285f4;"><img src="//cdn.mapkit.io/v1/icons/' +
-												icon +
-												'.svg"/></i><span><a href="mailto:' +
-												location.email +
-												'" target="_blank">' +
-												location.email +
-												'</a></span></div>'
-											);
-											break;
-										case 'web':
-											return (
-												'<div class="iw-details"><i class="material-icons" style="color:#4285f4;"><img src="//cdn.mapkit.io/v1/icons/' +
-												icon +
-												'.svg"/></i><span><a href="' +
-												location.web +
-												'" target="_blank">' +
-												location.web_formatted +
-												'</a></span></div>'
-											);
-											break;
-										case 'desc':
-											return (
-												'<label class="iw-desc" for="cb_details"><input type="checkbox" id="cb_details"/><h3 class="iw-x-details">Details</h3><i class="material-icons toggle-open-details"><img src="//cdn.mapkit.io/v1/icons/' +
-												icon +
-												'.svg"/></i><p class="iw-x-details">' +
-												location.desc +
-												'</p></label>'
-											);
-											break;
-										default:
-											return (
-												'<div class="iw-details"><i class="material-icons"><img src="//cdn.mapkit.io/v1/icons/' +
-												icon +
-												'.svg"/></i><span>' +
-												location.part +
-												'</span></div>'
-											);
-											break;
-									}
-									break;
-								case 'open_hours':
-									var items = '';
-									if (location.open_hours.length > 0) {
-										for (
-											var i = 0;
-											i < location.open_hours.length;
-											++i
-										) {
-											if (i !== 0) {
-												items +=
-													'<li><strong>' +
-													location.open_hours[i].day +
-													'</strong><strong>' +
-													location.open_hours[i].hours +
-													'</strong></li>';
-											}
-											var first =
-												'<li><label for="cb_hours"><input type="checkbox" id="cb_hours"/><strong>location.open_hours[0].day</strong><strong>' +
-												location.open_hours[0].hours +
-												'</strong><i class="material-icons toggle-open-hours"><img src="//cdn.mapkit.io/v1/icons/keyboard_arrow_down.svg"/></i><ul>' +
-												items +
-												'</ul></label></li>';
-										}
-										return (
-											'<div class="iw-list"><i class="material-icons first-material-icons" style="color:#4285f4;"><img src="//cdn.mapkit.io/v1/icons/' +
-											icon +
-											'.svg"/></i><ul>' +
-											first +
-											'</ul></div>'
-										);
-									} else {
-										return '';
-									}
-									break;
-							}
-						} else {
-							return '';
-						}
-					}
-					boxText.innerHTML =
-						buildPieces(location, 'photo', 'photo', '') +
-						buildPieces(location, 'iw-toolbar', 'title', '') +
-						buildPieces(location, 'div', 'address', 'location_on') +
-						buildPieces(location, 'div', 'web', 'public') +
-						buildPieces(location, 'div', 'email', 'email') +
-						buildPieces(location, 'div', 'tel', 'phone') +
-						buildPieces(location, 'div', 'int_tel', 'phone') +
-						buildPieces(
-							location,
-							'open_hours',
-							'open_hours',
-							'access_time'
-						) +
-						buildPieces(location, 'div', 'desc', 'keyboard_arrow_down');
-					// var myOptions = {
-					// 	alignBottom: true,
-					// 	content: boxText,
-					// 	disableAutoPan: false,
-					// 	maxWidth: 0,
-					// 	pixelOffset: new google.maps.Size(-140, -40),
-					// 	zIndex: null,
-					// 	boxStyle: {
-					// 		opacity: 1,
-					// 		width: '280px'
-					// 	},
-					// 	closeBoxMargin: '0px 0px 0px 0px',
-					// 	infoBoxClearance: new google.maps.Size(1, 1),
-					// 	isHidden: false,
-					// 	pane: 'floatPane',
-					// 	enableEventPropagation: false
-					// };
-					// location.ib = new InfoBox(myOptions);
-					// location.ib.open(map, marker);
-					location.infoWindowVisible = true;
-				}
-			});
-		}
 
 		function init() {
 			let mapOptions = {
@@ -797,7 +634,6 @@ get_header(); ?>
 					lng: 2.2962691000000177,
 					photo: '',
 					open_hours: '',
-					// TODO: change to local url
 					marker: {
                         url: '<?php echo get_template_directory_uri() . '/layoutImg/marker_full.png' ?>',
                         anchor: new google.maps.Point(51, 95)
@@ -842,10 +678,6 @@ get_header(); ?>
 				});
 
 				markersArray.push(marker);
-
-				if (locations[i].iw.enable === true) {
-					bindInfoWindow(marker, map, locations[i]);
-				}
 			}
 
 			var myoverlay = new google.maps.OverlayView();
