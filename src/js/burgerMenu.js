07ee1f1f53fe123d3ec91570fee691f76e7de5f8
Alphability <albanmezino@gmail.com>
2018-07-31 09:32:06 +0200

    🎨js new eslint
    Signed-off-by: Alphability <albanmezino@gmail.com>

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -3,19 +3,23 @@ const $ = require('jquery-slim');
 module.exports = function burgerMenu(header, burger) {
     if (!burger.length) return;
 
-    burger.on('click', (e) => {
+    burger.on('click', e => {
         e.preventDefault();
 
         if (header.hasClass('menu-open')) {
             const menuItemsWithChildren = $('.menu-item-has-children');
 
-            TweenLite.to($('.menu-item-has-children.on').find('.sub-menu-wrap'), 0.3, {
-                css: { maxHeight: 0 },
-                ease: Power4.easeOut,
-                onComplete: () => {
-                    header.toggleClass('menu-open');
-                },
-            });
+            TweenLite.to(
+                $('.menu-item-has-children.on').find('.sub-menu-wrap'),
+                0.3,
+                {
+                    css: { maxHeight: 0 },
+                    ease: Power4.easeOut,
+                    onComplete: () => {
+                        header.toggleClass('menu-open');
+                    },
+                }
+            );
 
             menuItemsWithChildren.removeClass('on').removeClass('was-on');
         } else {
