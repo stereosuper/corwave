4d3d96308cdd938272099b36f337d817ad460593
Alphability <albanmezino@gmail.com>
2018-08-09 16:51:36 +0200

    🎨loco translate
    Signed-off-by: Alphability <albanmezino@gmail.com>

--- dest/wp-content/themes/corwave/functions.php
+++ dest/wp-content/themes/corwave/functions.php
@@ -795,5 +795,13 @@ function getCurrentBlogLanguage() {
     return $language;
 }
 
+/*-----------------------------------------------------------------------------------*/
+/* I18n
+/*-----------------------------------------------------------------------------------*/
+
+add_action( 'after_setup_theme', 'my_theme_setup' );
+function my_theme_setup(){
+    load_theme_textdomain('concord', content_url() . '/languages/loco/themes');
+}
 
 ?>
