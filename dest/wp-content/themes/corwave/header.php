6f88db121c6583a25109fdf0bdb9b4293f680cec
Clément Lemoine <clementlemoine.cl@gmail.com>
2018-10-02 09:49:56 +0200

    🎨hotjar on homepage

--- dest/wp-content/themes/corwave/header.php
+++ dest/wp-content/themes/corwave/header.php
@@ -38,6 +38,20 @@ Text Domain: corwave
 		<link href="https://fonts.googleapis.com/css?family=Lato:100i,300,300i,400,400i,700" rel="stylesheet">
 
 		<script>document.getElementsByTagName('html')[0].className = 'js';</script>
+
+        <?php if(is_front_page()): ?>
+            <!-- Hotjar Tracking Code for www.corwave.com -->
+            <script>
+                (function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:1033996,hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            </script>
+        <?php endif; ?>
 	</head>
 
 	<body <?php body_class(); ?>>
